7a6891656681de0db76ff2de3a646167ddf8a8ed
Yuriy <osoftteam@yahoo.com>
2016-11-17 15:18:19 -0500

    cleaned up commented code

--- src/dropbox/DropboxRouteBase.h
+++ src/dropbox/DropboxRouteBase.h
@@ -11,29 +11,6 @@ namespace dropboxQt{
     public:
         DropboxRouteBase(Endpoint* ep):m_end_point(ep){}
 
-	protected:
-		/*
-		void bcall4VoidArgVoidRes(std::function<void(std::function<void()>,
-			std::function<void(std::unique_ptr<DropboxException>)>)> asyncFunction)
-		{
-			std::unique_ptr<DropboxException> ex;
-			asyncFunction
-			(
-				[&](void)
-			{
-				m_end_point->exitEventsLoop();
-			},
-				[&](std::unique_ptr<DropboxException> e)
-			{
-				ex = std::move(e);
-				m_end_point->exitEventsLoop();
-			}
-			);
-			m_end_point->runEventsLoop();
-			if (ex)
-				ex->raise();
-		};
-		*/
     protected:
         Endpoint* m_end_point;
     };
